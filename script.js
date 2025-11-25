e9f205d4fd549a9e2cff314ec6d919414543fd6b
Ryan Karakoc <ryan.karakoc@quals-direct.co.uk>
2025-11-25 15:58:59 +0000

    changed post to get

--- script.js
+++ script.js
@@ -244,8 +244,8 @@ async function testProcessPayment(paymentData) {
             //}).then((res) => res.json());
             //return { transactionState: "SUCCESS" };
 
-            const response = await fetch(`https://my.npors.com/npors/ajax/paypal/capture_payment_sandbox.asp?pl_id=${id}`, {
-                method: 'POST',
+            const response = await fetch(`https://my.npors.com/npors/ajax/paypal/capture_payment_sandbox.asp`, {
+                method: 'GET',
             }).then((res) => {
                 console.log(res)
                 console.log(res.json())
