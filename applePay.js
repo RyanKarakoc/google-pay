09fdbf37993f25521482a06364b58d54afa79c19
Ryan Karakoc <ryan.karakoc@quals-direct.co.uk>
2026-01-06 14:07:16 +0000

    added pay session

--- applePay.js
+++ applePay.js
@@ -1,4 +1,5 @@
-﻿if (!window.ApplePaySession) {
+﻿// SET UP APPLE PAY BUTTON
+if (!window.ApplePaySession) {
     console.error('This device does not support Apple Pay');
 }
 if (!ApplePaySession.canMakePayments()) {
@@ -14,3 +15,75 @@ applepay.config()
     .catch(applepayConfigError => {
         console.error('Error while fetching Apple Pay configuration.');
     });
+
+
+// CREATE APPLE PAY SESSION
+const paymentRequest = {
+    countryCode: applepayConfig.countryCode,
+    merchantCapabilities: applepayConfig.merchantCapabilities,
+    supportedNetworks: applepayConfig.supportedNetworks,
+    currencyCode: "USD",
+    requiredShippingContactFields: ["name", "phone", "email", "postalAddress"],
+    requiredBillingContactFields: ["postalAddress"],
+    total: {
+        label: "Demo",
+        type: "final",
+        amount: "10.00",
+    }
+};
+const session = new ApplePaySession(4, paymentRequest);
+
+// CALLBACK onvalidatemerchant
+session.onvalidatemerchant = (event) => {
+    applepay.validateMerchant({
+        validationUrl: event.validationURL,
+        displayName: "My Store"
+    })
+        .then(validateResult => {
+            session.completeMerchantValidation(validateResult.merchantSession);
+        })
+        .catch(validateError => {
+            console.error(validateError);
+            session.abort();
+        });
+};
+
+// CALLBACK onpaymentauthorized 
+session.onpaymentauthorized = (event) => {
+    console.log('Your billing address is:', event.payment.billingContact);
+    console.log('Your shipping address is:', event.payment.shippingContact);
+    fetch("/api/orders", {
+        method: 'post',
+        body: {}
+    })
+        .then(res => res.json())
+        .then((createOrderData) => {
+            var orderId = createOrderData.id;
+            applepay.confirmOrder({
+                orderId: orderId,
+                token: event.payment.token,
+                billingContact: event.payment.billingContact
+            })
+                .then(confirmResult => {
+                    session.completePayment(ApplePaySession.STATUS_SUCCESS);
+                    fetch(`/api/orders/${orderId}/capture`, {
+                        method: "post",
+                    })
+                        .then(res => res.json())
+                        .then(captureResult => {
+                            console.log(captureResult);
+                        })
+                        .catch(captureError => console.error(captureError));
+                })
+                .catch(confirmError => {
+                    if (confirmError) {
+                        console.error('Error confirming order with applepay token');
+                        console.error(confirmError);
+                        session.completePayment(ApplePaySession.STATUS_FAILURE);
+                    }
+                });
+        });
+};
+
+// SHOW PAYMENT SHEET
+session.begin();
