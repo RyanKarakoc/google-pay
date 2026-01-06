52730805c88db6136f22687e76f6cf5041f4d574
Ryan Karakoc <ryan.karakoc@quals-direct.co.uk>
2026-01-06 13:55:15 +0000

    added apple pay scripts

--- applePay.js
+++ applePay.js
@@ -1,1 +1,16 @@
-﻿console.log("hello")
+﻿if (!window.ApplePaySession) {
+    console.error('This device does not support Apple Pay');
+}
+if (!ApplePaySession.canMakePayments()) {
+    console.error('This device is not capable of making Apple Pay payments');
+}
+const applepay = paypal.Applepay();
+applepay.config()
+    .then(applepayConfig => {
+        if (applepayConfig.isEligible) {
+            document.getElementById("applepay-container").innerHTML = '<apple-pay-button id="btn-appl" buttonstyle="black" type="buy" locale="en">';
+        }
+    })
+    .catch(applepayConfigError => {
+        console.error('Error while fetching Apple Pay configuration.');
+    });
